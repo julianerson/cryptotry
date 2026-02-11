95fd8ad2484e1231af87aeaf6b46caef6577de47
kali <kali@kali.kali>
2026-02-11 12:07:03 +0000

    uns comentarios meus no codigo

--- cryptotry.py
+++ cryptotry.py
@@ -2,26 +2,26 @@ from InquirerPy import inquirer
 
 print('bem vindo ao encriptografador de ?trybulet?')
 
-def xor():
+def xor():#aqui temos uma cryptografia xor, ela pode encryptografar e desincryptar
     palavra = input("diga sua palavra-chave")
     key = input("diz a chave")
     key = int(key)
-    pos = "".join(chr(ord(o) ^ key)for o in palavra)
+    pos = "".join(chr(ord(o) ^ key)for o in palavra)#onde a magica acontece
     print(f"sua senha e {pos}")
-def cesar():
-    pos = []
+def cesar():#cifra de cesar aqui
+    pos = []#parte nescessaria para a identaçao
     print('obs: nao pode haver virgulas, e letras maiusculas viram minusculas')
     word = input("diga sua palavra")
-    word = word.lower()
-    key = int(input("diga a chave"))
+    word = word.lower()#por motivos de preguiça, eu nao adicionei letras maiusculas
+    key = int(input("diga a chave"))#chave para decodificar
     for o in word:
-        z = ord(o)
-        if 48 <= z <= 57:
+        z = ord(o)#transformaçao para unicode
+        if 48 <= z <= 57:#aqui a identificaçao de letras
             p = z + key
-            if 58 <= p:
+            if 58 <= p:#preciso melhorar, as vezes a chave e beem grande
                 p = p - 10
             lo = chr(p)
-        if 97 <= z <= 122:
+        if 97 <= z <= 122:#aqui a de numeros
             p = z + key
             if 123 <= p:
                 p = p - 26
